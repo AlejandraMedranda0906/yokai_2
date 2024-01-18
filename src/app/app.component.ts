a5b79aaa8310f1d3d35987b7e97b2bd34d5fffa1
Alejandra <146900779+AlejandraMedranda0906@users.noreply.github.com>
2024-01-17 22:45:37 -0500

    c1
    c1

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,11 +1,15 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterOutlet } from '@angular/router';
+import { CabezaComponent } from './components/cabeza/cabeza.component';
+import { CuerpoComponent } from './components/cuerpo/cuerpo.component';
+import { PiernasComponent } from './components/piernas/piernas.component';
+import { PiesComponent } from './components/pies/pies.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [CommonModule, RouterOutlet],
+  imports: [CommonModule, RouterOutlet,CabezaComponent,CuerpoComponent, PiernasComponent, PiesComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
